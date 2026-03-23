ca5ae11f897d453c3b830f71e9e960c29ba207b5
abu1221baker <abubakergit1221@gmail.com>
2026-03-23 23:35:14 +0530

    made the image source as media folder

--- core/urls.py
+++ core/urls.py
@@ -44,3 +44,7 @@ urlpatterns = [
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
